fe134e940d91dd43e8b94f8e388790bea71e3977
atari-monk <atari.monk1@gmail.com>
2023-08-28 00:08:18 +0200

    task routes

---
+++ web-api.tests/src/task/routes.ts
@@ -0,0 +1,58 @@
+import { HttpMethod, IRouting } from 'atari-monk-api-tester-api'
+
+export const getRoutes = (
+  url: string,
+  userId?: string,
+  taskId?: string,
+  projectId?: string
+): IRouting => {
+  return {
+    baseUrl: url,
+    endpoints: {
+      createUser: {
+        method: HttpMethod.POST,
+        endpoint: 'users',
+      },
+      createUserProject: {
+        method: HttpMethod.POST,
+        endpoint: 'projects/create',
+      },
+      createTask: {
+        method: HttpMethod.POST,
+        endpoint: 'tasks',
+      },
+      getTasksForUser: {
+        method: HttpMethod.GET,
+        endpoint: `tasks/user/${userId}`,
+      },
+      getTasksForUserAndProject: {
+        method: HttpMethod.GET,
+        endpoint: `tasks/user/${userId}/${projectId}`,
+      },
+      updateTask: {
+        method: HttpMethod.PATCH,
+        endpoint: `tasks/${taskId}`,
+      },
+      getAllTasks: {
+        method: HttpMethod.GET,
+        endpoint: 'tasks/all',
+      },
+      finishTask: {
+        method: HttpMethod.PATCH,
+        endpoint: `tasks/finish/${taskId}`,
+      },
+      deleteUser: {
+        method: HttpMethod.DELETE,
+        endpoint: `users/${userId}`,
+      },
+      deleteProject: {
+        method: HttpMethod.DELETE,
+        endpoint: `projects/${projectId}`,
+      },
+      deleteTask: {
+        method: HttpMethod.DELETE,
+        endpoint: `tasks/${taskId}`,
+      },
+    },
+  }
+}
