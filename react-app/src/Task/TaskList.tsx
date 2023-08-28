2d3dba4644f7de7ad06115abd49eddad07d29b45
atari-monk <atari.monk1@gmail.com>
2023-08-28 19:57:31 +0200

    reorder tast view

--- react-app/src/Task/TaskList.tsx
+++ react-app/src/Task/TaskList.tsx
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { StyledTaskList } from '../styles';
-import ITask from './ITask';
-import ITaskListProps from './ITaskListProps';
-import { AuthContext } from '../Auth/AuthProvider';
-import ProjectSelection from '../Project/ProjectSelection';
-import IProject from '../Project/IProject';
-import TaskPopup from './TaskPopup';
-import Modal from '../components/ModalOverlay';
-import IAppConfig from '../config/IAppConfig';
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import { StyledTaskList } from '../styles'
+import ITask from './ITask'
+import ITaskListProps from './ITaskListProps'
+import { AuthContext } from '../Auth/AuthProvider'
+import ProjectSelection from '../Project/ProjectSelection'
+import IProject from '../Project/IProject'
+import TaskPopup from './TaskPopup'
+import Modal from '../components/ModalOverlay'
+import IAppConfig from '../config/IAppConfig'
 
 const TaskList: React.FC<ITaskListProps> = ({ config }) => {
-  const [tasks, setTasks] = useState<ITask[]>([]);
-  const { userId } = useContext(AuthContext);
-  const [projects, setProjects] = useState<IProject[]>([]);
-  const [selectedProjectId, setSelectedProjectId] = useState('');
-  const [showPopup, setShowPopup] = useState(false);
-  const [taskId, setTaskId] = useState('');
+  const [tasks, setTasks] = useState<ITask[]>([])
+  const { userId } = useContext(AuthContext)
+  const [projects, setProjects] = useState<IProject[]>([])
+  const [selectedProjectId, setSelectedProjectId] = useState('')
+  const [showPopup, setShowPopup] = useState(false)
+  const [taskId, setTaskId] = useState('')
 
   const fetchProjects = async (
     userId: string,
@@ -24,15 +24,15 @@ const TaskList: React.FC<ITaskListProps> = ({ config }) => {
     setProjects: React.Dispatch<React.SetStateAction<IProject[]>>
   ) => {
     try {
-      console.log('Fetching projects...');
+      console.log('Fetching projects...')
       const response = await axios.get<IProject[]>(
         `${config.apiUrl}/projects/user?userId=${userId}`
-      );
-      setProjects(response.data);
+      )
+      setProjects(response.data)
     } catch (error) {
-      console.error('Failed to fetch projects:', error);
+      console.error('Failed to fetch projects:', error)
     }
-  };
+  }
 
   const fetchTasks = async (
     userId: string,
@@ -41,56 +41,56 @@ const TaskList: React.FC<ITaskListProps> = ({ config }) => {
     setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
   ) => {
     try {
-      console.log('Fetching tasks...');
-      console.log('Selected Project ID:', selectedProjectId);
+      console.log('Fetching tasks...')
+      console.log('Selected Project ID:', selectedProjectId)
       const response = await axios.get<ITask[]>(
         `${config.apiUrl}/tasks/user/${userId}/${selectedProjectId}`
-      );
-      setTasks(response.data);
-      console.log(`${config.apiUrl}/tasks/user/${userId}/${selectedProjectId}`);
-      console.log(response.data);
+      )
+      setTasks(response.data)
+      console.log(`${config.apiUrl}/tasks/user/${userId}/${selectedProjectId}`)
+      console.log(response.data)
     } catch (error) {
-      console.error('Failed to fetch tasks:', error);
+      console.error('Failed to fetch tasks:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    const savedSelectedProjectId = localStorage.getItem('selectedProjectId');
+    const savedSelectedProjectId = localStorage.getItem('selectedProjectId')
     if (savedSelectedProjectId) {
-      setSelectedProjectId(savedSelectedProjectId);
+      setSelectedProjectId(savedSelectedProjectId)
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    console.log('Selected Project ID:', selectedProjectId);
-    fetchProjects(userId, config, setProjects);
-  }, [config, selectedProjectId, userId]);
+    console.log('Selected Project ID:', selectedProjectId)
+    fetchProjects(userId, config, setProjects)
+  }, [config, selectedProjectId, userId])
 
   useEffect(() => {
     if (selectedProjectId) {
-      console.log('Fetching tasks...');
-      console.log('Selected Project ID:', selectedProjectId);
-      fetchTasks(userId, selectedProjectId, config, setTasks);
+      console.log('Fetching tasks...')
+      console.log('Selected Project ID:', selectedProjectId)
+      fetchTasks(userId, selectedProjectId, config, setTasks)
     }
-  }, [config, selectedProjectId, userId]);
+  }, [config, selectedProjectId, userId])
 
   const finishTask = async (taskId: string) => {
     try {
-      setTaskId(taskId);
-      setShowPopup(true);
+      setTaskId(taskId)
+      setShowPopup(true)
     } catch (error) {
-      console.error('Failed to finish task:', error);
+      console.error('Failed to finish task:', error)
     }
-  };
+  }
 
   const handlePopupFinish = () => {
-    setShowPopup(false);
-  };
+    setShowPopup(false)
+  }
 
   const handleProjectChange = (projectId: string) => {
-    setSelectedProjectId(projectId);
-    localStorage.setItem('selectedProjectId', projectId);
-  };
+    setSelectedProjectId(projectId)
+    localStorage.setItem('selectedProjectId', projectId)
+  }
 
   return (
     <>
@@ -111,10 +111,7 @@ const TaskList: React.FC<ITaskListProps> = ({ config }) => {
       <StyledTaskList>
         {tasks.map((task) => (
           <div key={task._id}>
-            <h3>{task.description}</h3>
             <p>Created At: {task.localTimestamp}</p>
-            {task.summary && <p>Summary: {task.summary}</p>}
-            {task.finishedAt && <p>Finished At: {task.finishLocalTimestamp}</p>}
             {!task.finishedAt && (
               <>
                 <button onClick={() => finishTask(task._id)}>
@@ -122,12 +119,14 @@ const TaskList: React.FC<ITaskListProps> = ({ config }) => {
                 </button>
               </>
             )}
-            <hr />
+            {task.finishedAt && <p>Finished At: {task.finishLocalTimestamp}</p>}
+            <p>Task: {task.description}</p>
+            {task.summary && <p>Summary: {task.summary}</p>}
           </div>
         ))}
       </StyledTaskList>
     </>
-  );
-};
+  )
+}
 
-export default TaskList;
+export default TaskList
