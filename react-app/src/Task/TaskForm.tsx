e757fd60f1bdf1b99f02c3c428baa8a8260b7754
atari-monk <atari.monk1@gmail.com>
2023-08-30 13:34:14 +0200

    format

--- react-app/src/Task/TaskForm.tsx
+++ react-app/src/Task/TaskForm.tsx
@@ -1,45 +1,45 @@
-import React, { useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { StyledTaskForm } from '../styles';
-import { AuthContext } from '../Auth/AuthProvider';
-import ITaskFormProps from './ITaskFormProps';
-import IProject from '../Project/IProject';
-import ProjectSelection from '../Project/ProjectSelection';
+import React, { useContext, useState, useEffect } from 'react'
+import axios from 'axios'
+import { StyledTaskForm } from '../styles'
+import { AuthContext } from '../Auth/AuthProvider'
+import ITaskFormProps from './ITaskFormProps'
+import IProject from '../Project/IProject'
+import ProjectSelection from '../Project/ProjectSelection'
 
 const TaskForm: React.FC<ITaskFormProps> = ({ config }) => {
-  const [description, setDescription] = useState('');
-  const [projects, setProjects] = useState<IProject[]>([]);
-  const [selectedProjectId, setSelectedProjectId] = useState('');
-  const { userId } = useContext(AuthContext);
+  const [description, setDescription] = useState('')
+  const [projects, setProjects] = useState<IProject[]>([])
+  const [selectedProjectId, setSelectedProjectId] = useState('')
+  const { userId } = useContext(AuthContext)
 
   useEffect(() => {
     async function fetchProjects() {
       try {
         const response = await axios.get<IProject[]>(
           `${config.apiUrl}/projects/user?userId=${userId}`
-        );
-        setProjects(response.data);
+        )
+        setProjects(response.data)
       } catch (error) {
-        console.error('Failed to fetch projects:', error);
+        console.error('Failed to fetch projects:', error)
       }
     }
-    fetchProjects();
-  }, [config.apiUrl, userId]);
+    fetchProjects()
+  }, [config.apiUrl, userId])
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
       await axios.post(`${config.apiUrl}/tasks`, {
         description,
         projectId: selectedProjectId,
         userId,
-      });
-      setDescription('');
-      setSelectedProjectId('');
+      })
+      setDescription('')
+      setSelectedProjectId('')
     } catch (error) {
-      console.error('Failed to create task:', error);
+      console.error('Failed to create task:', error)
     }
-  };
+  }
 
   return (
     <StyledTaskForm onSubmit={handleSubmit}>
@@ -50,18 +50,6 @@ const TaskForm: React.FC<ITaskFormProps> = ({ config }) => {
         required
         placeholder="Enter task description"
       />
-      {/* <select
-        value={selectedProjectId}
-        onChange={(e) => setSelectedProjectId(e.target.value)}
-        required
-      >
-        <option value="">Select a project</option>
-        {projects.map((project) => (
-          <option key={project._id} value={project._id}>
-            {project.name}
-          </option>
-        ))}
-      </select> */}
       <ProjectSelection
         projects={projects}
         selectedProjectId={selectedProjectId}
@@ -69,7 +57,7 @@ const TaskForm: React.FC<ITaskFormProps> = ({ config }) => {
       />
       <button type="submit">Add Task</button>
     </StyledTaskForm>
-  );
-};
+  )
+}
 
-export default TaskForm;
+export default TaskForm
